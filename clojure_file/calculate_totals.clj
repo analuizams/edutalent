8e7c35bd69be06eac2da1bea156523c150e98346
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2023-08-03 15:33:40 -0300

    add clojure file

--- clojure_file/calculate_totals.clj
+++ clojure_file/calculate_totals.clj
@@ -11,4 +11,8 @@
      {:product-name "p2" :price 1.2 :quantity 7  :total 8.4}
      {:product-name "p3" :price 0.3 :quantity 2  :total 0.6}])
 
-(println(calculate-total input))
+(def result (calculate-total input))
+
+(println result)
+
+result
